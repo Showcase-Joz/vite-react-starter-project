eeb023081867eaa750bb56d7fc5589ffaabe8e0f
Joz <jolyon.favreau@smartsheet.com>
2023-06-29 21:51:41 +0100

    removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-=======
 import { useState } from "react";
 import { QrCode } from "./components/index";
 import reactLogo from "./assets/react.svg";
@@ -15,7 +6,6 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
->>>>>>> 97c08b2 (git init, first test - create a QR code that regererates based on a uID)
 
   return (
     <>
@@ -28,28 +18,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-<<<<<<< HEAD
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
-=======
       <QrCode viteLogo={viteLogo}></QrCode>
     </>
   );
 }
 
 export default App;
->>>>>>> 97c08b2 (git init, first test - create a QR code that regererates based on a uID)
