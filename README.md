7597e763fc85515ec05e0617b22cb8efcea5579f
Joz <jolyon.favreau@smartsheet.com>
2024-04-08 21:43:46 +0100

    update dependancies, create readme and add buttons to example

---
+++ README.md
@@ -0,0 +1,14 @@
+## Bare Bones
+
+this is a boilerplate starting project using the following:
+
+- pnpm | package manager
+- vite | dev server
+- eslint | code analysis
+- styled components | component css
+
+### Commands
+all commands are prefixed `pnpm`:
+- dev | runs the local server
+- build | runs the local server and builds files to /dist
+- preview | builds files to /dist and runs a preview of that
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { QrCode } from "./components/index";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -7,18 +6,26 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  const handleClickUp = () => {
+    setCount(count + 1);
+  };
+  const handleClickDown = () => {
+    setCount(count - 1);
+  };
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <QrCode viteLogo={viteLogo}></QrCode>
+      <button onClick={handleClickUp}>+</button>
+      <h3>{count}</h3>
+      <button onClick={handleClickDown}>-</button>
     </>
   );
 }
